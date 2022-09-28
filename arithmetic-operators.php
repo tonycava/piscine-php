d40c4e703aac38c3d808a0b7b4a7d4fffb8397e1
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-28 08:48:59 +0200

    test arithmetic-operators.php 6

--- arithmetic-operators.php
+++ arithmetic-operators.php
@@ -4,7 +4,7 @@ $firstNbr = 100;
 $secondNbr = 35;
 
 $firstSum = $firstNbr + $secondNbr;
-$firstSum = $firstNbr - $secondNbr;
+$firstSub  = $firstNbr - $secondNbr;
 $firstMult = $firstNbr * $secondNbr;
 $firstDivision = $firstNbr / $secondNbr;
 $firstModulus = $firstNbr % $secondNbr;
