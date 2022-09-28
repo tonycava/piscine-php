0676dc12df1b7971b09ae7828400491d697c6ec3
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-28 09:32:18 +0200

    test its-ricard-time.php 4

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -2,10 +2,10 @@
 $nbrRicard = 1;
 do {
     if ($nbrRicard === 7){
-        echo "Come on number $nbrRicard";
+        echo "Come on ricard number $nbrRicard";
         break;
     }
-    echo "Come on number $nbrRicard\n";
+    echo "Come on ricard number $nbrRicard\n";
     if ($nbrRicard === 3) echo "I'll have to stop soon!\n";
     if ($nbrRicard === 5) echo "I am no longer very fresh ...\n";
     $nbrRicard++;
