32c435ea1aac814c37f773bbc15135fff59ec804
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-28 09:12:33 +0200

    test be-polite.php 5

--- be-polite.php
+++ be-polite.php
@@ -1,10 +1,11 @@
 <?php
+
 $hour = date("H");
 if ($hour >= 6 and $hour <= 12) {
     echo "Hello! Have a nice day :)";
-} else if ($hour > 12 and $hour <= 18) {
+} elseif ($hour > 12 and $hour <= 18) {
     echo "Have a good afternoon!";
-} else if ($hour > 18 and $hour <= 21) {
+} elseif ($hour > 18 and $hour <= 21) {
     echo "Good evening! Hope you had a good day!";
 } else {
     echo "Good night! See you tomorrow :)";
---
+++ multiple-table.php
@@ -0,0 +1,6 @@
+<?php
+$multiplyBy = 1;
+while ($multiplyBy != 10) {
+    $stock = 9 * $multiplyBy;
+    echo "9 * $multiplyBy = $stock";
+}
