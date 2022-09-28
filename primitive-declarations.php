a83ad99c67a7a780ed0b661b2b1e061619d5a862
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-28 08:35:38 +0200

    first commit

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstStr = "Hi!";
+$firstEscapeStr = "\n";
+$firstBool = false;
+$firstInteger = 2;
+$firstFloat = 5.5;
+$firstNull = null;
+$firstArray = [$firstInteger, $firstNull, $firstStr, $firstFloat, $firstBool];
