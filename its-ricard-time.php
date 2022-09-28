1a63794c6bd51529a1731049e13176618275b907
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-28 09:23:48 +0200

    test its-ricard-time.php 1

---
+++ its-ricard-time.php
@@ -0,0 +1,8 @@
+<?php
+$nbrRicard = 1;
+do {
+    echo "Come on ricard $nbrRicard\n";
+    if ($nbrRicard === 3) echo "I'll have to stop soon!\n";
+    if ($nbrRicard === 5) echo "I am no longer very fresh ...\n";
+    $nbrRicard++;
+} while ($nbrRicard != 8);
