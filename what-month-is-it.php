13f440fddca7e6606aee82e880afcf7681e22c7c
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-28 09:54:47 +0200

    test what-month-is-it.php 1

---
+++ what-month-is-it.php
@@ -0,0 +1,16 @@
+<?php
+$month = 1;
+$test_array = array(1 => "January",
+                    2 => "February",
+                    3 => "March",
+                    4 => "April",
+                    5 => "May",
+                    6 => "June",
+                    7 => "July",
+                    8 => "August",
+                    9 => "September",
+                    10 => "October",
+                    11 => "November",
+                    12 => "December",
+);
+echo  "We are in the month of " . $test_array[$month];
