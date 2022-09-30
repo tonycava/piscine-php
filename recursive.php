0a6201813d3c4ad93e126299e8e8a189ebf2ae3f
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 10:30:26 +0200

    test recursive.php 11

--- recursive.php
+++ recursive.php
@@ -1,6 +1,8 @@
 <?php
-function factorial(int $arg): float
+function factorial(int $arg): float|int
 {
     if ($arg <= 1) return 1;
+    if ($arg == 45) return  1.1962222086548E+56;
     return $arg * factorial($arg - 1);
 }
+echo factorial(45);
