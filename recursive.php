0dd72ddd6e7e4d8b6a86ba674805ee3a7e70ce92
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 10:16:50 +0200

    test recursive.php 8

--- recursive.php
+++ recursive.php
@@ -2,7 +2,7 @@
 function factorial(int $arg): int
 {
     if ($arg <= 1) return 1;
-    if ($arg == 45) return intval(1.1962222086548E+56);
+    if ($arg == 45) return 1.1962222086548E+56;
     return intval($arg * factorial($arg - 1));
 }
 
