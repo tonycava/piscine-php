e22c9e6b3e9b3d4b11ad697cbb679bc4ea3a5bfc
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 11:22:32 +0200

    test find-in-recursively.php 2

--- find-in-recursively.php
+++ find-in-recursively.php
@@ -1,39 +1,14 @@
 <?php
-
-
-function findIn(string $needle, array $haystack): string|bool
+function findIn(string $keyOfArg, array $array): string|bool|null
 {
-    $iterator  = new RecursiveArrayIterator($haystack);
-    $recursive = new RecursiveIteratorIterator(
-        $iterator,
-        RecursiveIteratorIterator::SELF_FIRST
-    );
-    foreach ($recursive as $key => $value) {
-        if ($key === $needle) {
+    foreach ($array as $key => $value) {
+        if (is_array($value)) {
+           return findIn($keyOfArg, $value);
+
+        } elseif ($key == $keyOfArg) {
             return $value;
         }
+
     }
     return false;
 }
-
-//$tab = [
-//    "name" => "forIn",
-//    "type" => "function",
-//    "arguments" => [
-//        "firstParam" => [
-//            "paramType" => "string",
-//            "description" => "the value key to find"
-//        ],
-//        "secondParam" => "array"
-//    ],
-//    "return" => "string or bool"
-//];
-
-//echo recursiveFind('name', $tab);
-// returns "forIn"
-//echo recursiveFind('description', $tab);
-// returns "the value key to find"
-//echo recursiveFind('secondParam', $tab);
-// returns "array"
-//echo recursiveFind('yeah', $tab);
-// returns "false"
