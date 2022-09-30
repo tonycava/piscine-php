2bad75c9e0b05a417b6c685ab33f75e0085eae54
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 09:30:51 +0200

    test calculator.php 2

--- calculator.php
+++ calculator.php
@@ -1,11 +1,22 @@
 <?php
 
-function calc(string $str): int {
-    $math_string ="print (".$str.");";
-    $result = eval($math_string);
-
-    echo $result;
-
-    return $result;
+function calc(string $str): int
+{
+    return matheval($str);
 }
 
+function matheval($equation){
+    $equation = preg_replace("/[^0-9+\-.*\/()%]/","",$equation);
+    $equation = preg_replace("/([+-])([0-9]+)(%)/","*(1\$1.\$2)",$equation);
+    // you could use str_replace on this next line
+    // if you really, really want to fine-tune this equation
+    $equation = preg_replace("/([0-9]+)(%)/",".\$1",$equation);
+    if ( $equation == "" ) {
+        $return = 0;
+    } else {
+        eval("\$return=" . $equation . ";" );
+    }
+    return $return;
+}
+
+echo calc("1+1");
