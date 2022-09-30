60a34e41b288bf0d4230f8c940e41ae9ad867e04
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 08:46:37 +0200

    test string-manipulator.php 1

---
+++ string-manipulator.php
@@ -0,0 +1,26 @@
+<?php
+function capsMe(string $str): string
+{
+    return strtoupper($str);
+}
+
+function lowerMe(string $str): string
+{
+    return strtolower($str);
+}
+
+function upperCaseFirst(string $str): string
+{
+    return ucfirst($str);
+}
+
+function lowerCaseFirst(string $str): string
+{
+    return lcfirst($str);
+
+}
+
+function removeBlankSpace(string $str): string
+{
+    return trim($str);
+}
