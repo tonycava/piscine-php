a66c453676d36d98a046a642bf1f461bcba580ae
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 12:20:14 +0200

    test thats-anonymous.php 1

---
+++ thats-anonymous.php
@@ -0,0 +1,17 @@
+<?php
+$date = "It is " . date('F t, Y');
+
+$today = function () use (&$date) {
+    return $date;
+};
+
+$my_year = date("Y");
+
+$isLeapYear = function ($my_year) {
+    if ($my_year % 4 == 0)
+        return true;
+    else
+        return false;
+};
+
+echo $today();
