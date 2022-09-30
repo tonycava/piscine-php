0fe0ad975e9ed9cc19c6909ded5153377f683a08
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 08:59:46 +0200

    test string-manipulator.php 3

--- string-manipulator.php
+++ string-manipulator.php
@@ -16,11 +16,21 @@ function upperCaseFirst(string $str): string
 
 function lowerCaseFirst(string $str): string
 {
-    return lcfirst($str);
-
+    $final = "";
+    $arr = explode(" ", $str);
+    foreach ($arr as $words) {
+        if (lcfirst($words[0]) === $words[0]) {
+            $final .= $words . " ";
+        } else {
+            $final .= lcfirst($words) . " ";
+        }
+    }
+    return trim($final);
 }
 
 function removeBlankSpace(string $str): string
 {
     return trim($str);
 }
+
+echo lowerCaseFirst("Hello BaBe");
