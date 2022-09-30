2b463e722852b42bb74ec6ede63859c719adcb0d
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 12:14:24 +0200

    test find-in-recursively.php 3

--- find-in-recursively.php
+++ find-in-recursively.php
@@ -1,14 +1,41 @@
 <?php
-function findIn(string $keyOfArg, array $array): string|bool|null
+function findIn(string $keyOfArg, array $array): string|bool
 {
+    $last = "";
     foreach ($array as $key => $value) {
-        if (is_array($value)) {
-           return findIn($keyOfArg, $value);
-
-        } elseif ($key == $keyOfArg) {
+        if ($key == $keyOfArg) {
             return $value;
         }
 
+        if (is_array($value)) {
+            $last = findIn($keyOfArg, $value);
+        }
     }
+    if ($last) return $last;
     return false;
 }
+
+$tab = [
+    "name" => "forIn",
+    "type" => "function",
+    "arguments" => [
+        "firstParam" => [
+            "paramType" => "string",
+            "description" => "the value key to find"
+        ],
+        "secondParam" => "array"
+    ],
+    "return" => "string or bool"
+];
+
+echo findIn("name", $tab) . "\n";
+// returns "forIn"
+
+echo findIn('description', $tab) . "\n";
+// returns "the value key to find"
+
+echo findIn('secondParam', $tab) . "\n";
+// returns "array"
+
+echo findIn('return', $tab) . "\n";
+// returns "string or bool";
