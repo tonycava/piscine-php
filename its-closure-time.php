d58fbf3090ca32f686cd96fc7b68b27b61af8cb8
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-30 13:19:33 +0200

    test its-closure-time.php 3

--- its-closure-time.php
+++ its-closure-time.php
@@ -1,11 +1,11 @@
 <?php
 
-function f() use(&$add)
+function f()
 {
     return array(
-        "+" => static fn (int $x, int $y) => $x + $y,
-        "-" => static fn (int $x, int $y) => $x - $y,
-        "*" => static fn (int $x, int $y) => $x * $y,
+        "+" => static fn(int $x, int $y) => $x + $y,
+        "-" => static fn(int $x, int $y) => $x - $y,
+        "*" => static fn(int $x, int $y) => $x * $y,
     );
 }
 
