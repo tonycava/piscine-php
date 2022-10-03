a23607e86ba447eecdff58e78111fa04da33bbe0
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 12:51:03 +0200

    filter-this-callable.php test 9

--- filter-this-callable.php
+++ filter-this-callable.php
@@ -1,35 +1,20 @@
 <?php
 
-function myArrayFilter(callable|array $func, array|callable $arr = []): array
+function myArrayFilter($arr, $func = null): array
 {
     $result = [];
 
-    if (count($func) > 0) return $arr;
-
-    if (count($func) > 0) {
-        foreach ($func as $ar) {
-            if ($func($ar)) $result[] = $ar;
-        }
+    if ($func == null) {
+        $func = function ($arg) {
+            return !!$arg;
+        };
     }
+
+    foreach ($arr as $ar) {
+        if ($func($ar)) $result[] = $ar;
+    }
+
 
     return $result;
 
 }
-
-function odd($var)
-{
-    return $var & 1;
-}
-
-function even($var)
-{
-    return !($var & 1);
-}
-
-$array1 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
-$array2 = [6, 7, 8, 9, 10, 11, 12];
-
-echo "Impair :\n";
-print_r(array_filter(["odd"], $array1));
-echo "Pair :\n";
-print_r(array_filter($array2, "even"));
