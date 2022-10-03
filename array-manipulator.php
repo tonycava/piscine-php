f14d038e1b9ce934d18ef953d1cccf8c7603a84d
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 08:19:31 +0200

    array-manipulator.php test 1

---
+++ array-manipulator.php
@@ -0,0 +1,26 @@
+<?php
+function reverse(array $arr): array
+{
+    return array_reverse($arr, true);
+}
+
+function push(array $arr, string...$new): int
+{
+    return count([$arr, ...$new]);
+}
+
+function sum(array $arr): int
+{
+    return (array_sum($arr));
+}
+
+function arrayContains(array $arr, int|string|float $value): mixed
+{
+    if (in_array($value, $arr)) return $value;
+    return "Nothing";
+}
+
+function merge(array $arr1, array $arr2, array $arr3)
+{
+    return array_merge($arr1, $arr2, $arr3);
+}
--- thats-anonymous.php
+++ thats-anonymous.php
@@ -1,13 +1,13 @@
 <?php
 $date = "It is " . date('F t, Y');
 
-$today = function () use (&$date) {
+$today = function () use ($date) {
     return $date;
 };
 
 $my_year = date("Y");
 
-$isLeapYear = function ($my_year) {
+$isLeapYear = function($my_year) {
     if ($my_year % 4 == 0)
         return true;
     else
