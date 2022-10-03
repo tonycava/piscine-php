7b7554f27d6f28983dde8f4e96975d0498ced6fb
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 10:54:36 +0200

    dna-difference.php test 4

--- dna-difference.php
+++ dna-difference.php
@@ -1,11 +1,13 @@
 <?php
 
 function dnaDiff(string $code, string $code2): int|bool {
+    if (strlen($code) < strlen($code2) || strlen($code) > strlen($code2)) return false;
     $total = 0;
     for ($i = 0; $i< strlen($code); $i++) {
         if ($code[$i] != $code2[$i]) $total += 1;
     }
-    return $total != 0 ? $total : false;
+    return $total;
 }
 
-echo dnaDiff('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT');
+echo dnaDiff('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT') . "\n"; // 7
+echo dnaDiff('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCTAZA'); // 7
