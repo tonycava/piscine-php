1183765693c081254ed75e1c246a018ed157cd6b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 12:56:07 +0200

    filter-this-callable.php test 11

--- filter-this-callable.php
+++ filter-this-callable.php
@@ -2,6 +2,9 @@
 
 function myArrayFilter($arr, $func = null)
 {
+    print_r($arr) . "\n";
+    print_r($func);
+
     $result = [];
 
     if ($func == null) {
@@ -16,33 +19,33 @@ function myArrayFilter($arr, $func = null)
 
     return $result;
 }
-
-$entry = [
-    0 => 'foo',
-    1 => false,
-    2 => -1,
-    3 => null,
-    4 => '',
-    5 => '0',
-    6 => 0,
-];
-
-print_r(myArrayFilter($entry));
-
-function odd($var)
-{
-    return $var & 1;
-}
-
-function even($var)
-{
-    return !($var & 1);
-}
-
-$array1 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
-$array2 = [6, 7, 8, 9, 10, 11, 12];
-
-echo "Impair :\n";
-print_r(array_filter($array1, "odd"));
-echo "Pair :\n";
-print_r(array_filter($array2, "even"));
+//
+//$entry = [
+//    0 => 'foo',
+//    1 => false,
+//    2 => -1,
+//    3 => null,
+//    4 => '',
+//    5 => '0',
+//    6 => 0,
+//];
+//
+//print_r(myArrayFilter($entry));
+//
+//function odd($var)
+//{
+//    return $var & 1;
+//}
+//
+//function even($var)
+//{
+//    return !($var & 1);
+//}
+//
+//$array1 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5];
+//$array2 = [6, 7, 8, 9, 10, 11, 12];
+//
+//echo "Impair :\n";
+//print_r(myArrayFilter($array1, "odd"));
+//echo "Pair :\n";
+//print_r(myArrayFilter($array2, "even"));
