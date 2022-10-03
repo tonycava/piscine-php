e65e84c7339f639080ffc641487666d2156a670b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 12:05:15 +0200

    map-this-callable.php test 11

--- map-this-callable.php
+++ map-this-callable.php
@@ -2,10 +2,24 @@
 
 function myArrayMap(callable|null $func, array $arr, array    ...$arr2)
 {
-
-    if ($func == null) return $arr;
-
     $result = [];
+
+    if ($func == null && $arr2 == null) return $arr;
+
+    if ($func == null && $arr2 != null) {
+        foreach ($arr as $ar) {
+            array_push($result, [$ar]);
+        }
+
+        for ($i = 0; $i < count($arr2); $i++) {
+            for ($j = 0; $j < count($arr2[$i]); $j++) {
+                array_push($result[$j], $arr2[$i][$j]);
+            }
+        }
+
+        return $result;
+    }
+
 
     foreach ($arr as $key => $value) {
         if (!is_array($value)) {
@@ -41,4 +55,5 @@ function myArrayMap(callable|null $func, array $arr, array    ...$arr2)
 }
 
 print_r(myArrayMap(null, [1, 2, 3, 4, 5]));
+print_r(myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']));
 print_r([1, 2, 3]);
