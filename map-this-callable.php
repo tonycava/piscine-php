a556b9ae808fcc7dba835720cc2be0df382736c1
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 11:35:20 +0200

    map-this-callable.php test 4

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,24 +1,46 @@
 <?php
 function myArrayMap(
     callable $func,
-    array    $arr
+    array    $arr,
+    array    ...$arr2
 )
 {
     $result = [];
 
     foreach ($arr as $key => $value) {
-
         $updated_key = $func($key);
+
         if (!is_array($value)) {
             $result[$updated_key] = $func($value);
 
         } else {
             $result[$updated_key] = myArrayMap(
                 $func,
-                $arr[$key]
+                $arr[$key],
+                $arr2
             );
         }
     }
+
+
+    if ($arr2 != null) {
+        foreach ($arr as $key => $value) {
+
+            $updated_key = $func($key);
+            if (!is_array($value)) {
+                $result[$updated_key] = $func($value);
+
+            } else {
+                $result[$updated_key] = myArrayMap(
+                    $func,
+                    $arr[$key],
+                    $arr2
+
+                );
+            }
+        }
+    }
+
 
     return $result;
 
