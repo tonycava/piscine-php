e120a0a19cada7f8d21d2db4ec38e605a77ae1ad
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-03 11:17:10 +0200

    map-this-callable.php test 1

---
+++ map-this-callable.php
@@ -0,0 +1,26 @@
+<?php
+function array_map_recursive(
+    callable $func,
+    array    $arr
+)
+{
+    $result = [];
+
+    foreach ($arr as $key => $value) {
+
+        $updated_key = $func($key);
+        if (!is_array($value)) {
+
+            $result[$updated_key] = $func($value);
+
+        } else {
+            $result[$updated_key] = array_map_recursive(
+                $func,
+                $arr[$key]
+            );
+        }
+    }
+
+    return $result;
+
+}
