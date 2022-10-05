2fb63620b22b52dc573872a0fce04480810510a9
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-05 10:27:37 +0200

    invert-binary-tree.php test 2

--- invert-binary-tree.php
+++ invert-binary-tree.php
@@ -8,34 +8,3 @@ function invertTree(BinaryNode|null $tree): BinaryNode|null
     $tree->left = invertTree($flag);
     return $tree;
 }
-
-
-class BinaryNode
-{
-    public ?int $value = null; // Node value
-    public ?BinaryNode $left = null; // Left child
-    public ?BinaryNode $right = null; // Right child
-
-    public function __construct(int $value)
-    {
-        $this->value = $value;
-    }
-}
-
-// Basic use
-$root = new BinaryNode(1);
-$rootLeftChild = new BinaryNode(2);
-$rootRightChild = new BinaryNode(3);
-
-$root->left = $rootLeftChild;
-$root->right = $rootRightChild;
-
-echo $root->value; // 1
-echo $root->left->value; // 2
-echo $root->right->value; // 3
-
-$invertedTree = invertTree($root);
-
-echo $invertedTree->value; // 1
-echo $invertedTree->left->value; // 3
-echo $invertedTree->right->value; // 2
