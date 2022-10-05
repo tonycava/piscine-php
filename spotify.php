e079da492886dba931f97a9c08767b37bab3ef10
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-10-05 11:01:37 +0200

    spotify.php test 1

---
+++ spotify.php
@@ -0,0 +1,76 @@
+<?php
+
+class Song {
+    public string $artist;
+    public string $title;
+    public string $duration;
+
+    public function __construct()
+    {
+    }
+
+}
+
+class Playlist
+{
+    public array $songs;
+    public int $totalMedias;
+
+    public function __construct()
+    {
+    }
+
+    public function __toString(): string
+    {
+        return "la";
+    }
+
+    public function addMedia() {}
+
+
+}
+
+
+class App
+{
+
+    private array $content;
+
+
+    public function start()
+    {
+        $this->readLine();
+
+        $this->write("d");
+    }
+
+    public function setContent(array $content)
+    {
+        $this->content = $content;
+    }
+
+    public function write(string $toWrite)
+    {
+
+
+        print_r("\n");
+    }
+
+    private function getContent(): array
+    {
+        return $this->content;
+    }
+
+
+    private function readLine(bool $asArray = false): array|bool|string
+    {
+        ob_start();
+        echo implode("", $this->getContent());
+        $data = ob_get_contents();
+        if ($asArray) {
+            $data = explode("\n", ob_get_contents());
+        }
+        ob_clean();
+        return $data;
+    }
+}
